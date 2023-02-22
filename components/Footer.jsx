91a7282d5641138f7867e35443646ac8c3285cbd
leonnn124 <leonnnx124@gmail.com>
2023-02-22 22:21:03 +0800

    div self-closing

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => (
     whileInView="show"
     className={`${style.paddings} py-8 relative`}
   >
-    <div className="footer-gradient"></div>
+    <div className="footer-gradient" />
     <div className={`${style.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
